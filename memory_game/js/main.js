8714dd1ee51909642682a8e9bc53cbe4617a14c8
iamconnaught <wdonnelly22@gmail.com>
2019-03-17 14:37:28 -0500

    added DOM events

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,22 +21,39 @@ var cards = [
 		}
 ];
 var cardsInPlay = [];
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 var checkForMatch = function(){
+	//document.getELementsById(cards[cardId.cardImage])[0].setAttribute(src, )
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else { console.log("Sorry, try again.");
+		alert("You found a match!");
+	} else { alert("Sorry, try again.");
 	}
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id')
+	this.setAttribute('src', cards[cardId].cardImage)
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
+	checkForMatch();
 }
-flipCard(0);
-flipCard(2);
-checkForMatch();
+
+
+
+
+
+createBoard();
